85e9cdcb32fa123b4f194f866c47530b61e5e7ff
seanfitz121 <63353392+seanfitz121@users.noreply.github.com>
2022-11-21 16:49:43 +0000

    Draft observer design pattern

---
+++ CS4125/src/main/java/com/project/CS4125/model/Subject.java
@@ -0,0 +1,7 @@
+package com.project.CS4125.model;
+
+public interface Subject {
+    public void attach(Observer o);
+    public void detach(Observer o);
+    public void notifyUpdate(Vehicle v);
+}
