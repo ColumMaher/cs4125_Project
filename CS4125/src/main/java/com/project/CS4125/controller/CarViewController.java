79d15c35709e21b473d25676508ce2e8f45d205f
ColumMaher <colummaher1@gmail.com>
2022-12-01 17:17:04 +0000

    Basic Login/Register Style

--- CS4125/src/main/java/com/project/CS4125/controller/CarViewController.java
+++ CS4125/src/main/java/com/project/CS4125/controller/CarViewController.java
@@ -7,7 +7,6 @@ import com.project.CS4125.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
